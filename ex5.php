c848be319a6dec171ee647266ae62a296f8f1e92
MariaSoares9 <maria.c.silva28@edu.senai.br>
2026-09-10 16:01:12 -0300

    Delete ex5.php

--- ex5.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$lista = ["14 997379447", "11 40028922", "14 999055201", "14 998307471", "996677818"];
-$soma = 0;
-foreach ($lista as $n) {
-    $soma += (int)$n;
-}
-echo "A soma dos números é: " . $soma;
-?>  
