7d1f1852548a12b63305188a2ef15af3d87ac981
MariaSoares9 <maria.c.silva28@edu.senai.br>
2026-09-10 15:54:06 -0300

    Add files via upload

---
+++ ex2.php
@@ -0,0 +1,11 @@
+<?php
+
+$seguidores = ["ana_vsoares", "_dudamoraesg", "karinasoares36"];
+$soma = 0;
+
+foreach ($seguidores as $pessoas) {
+    echo "$pessoas <br>";
+}
+
+echo "A primeira posição da lista é: " . $seguidores[0];
+?>
